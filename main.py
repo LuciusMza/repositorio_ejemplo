d289da711f66cd3f415259e19ae13ffa8c870325
Luciano Domínguez <dominguezluciano2011@gmail.com>
2023-04-20 20:38:56 -0300

    solucionado error anterior

---
+++ main.py
@@ -0,0 +1,11 @@
+#para crear un archivo con git se debe empezar creando el repositorio con git init
+
+# después se configura el archivo y se pone git add y el nombre del archivo, para que el repositorio quede en la zona de transición ()
+
+# por último, para que el cambio quede definido de forma permanente tenemos que poner git commit
+
+# git log se usa para mostrar cuáles fueron los cambios y quién los hizo
+
+#git status muestra cuál es el estado de los archivos modificados (si están agregados o en transición)
+
+# para crear ramas se usa el comando git branch (para mos)
